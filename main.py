1a469c897ea6879aafa840eb1c9bf6195f1d1ac9
hectorjak <hectorheltj@gmail.com>
2026-09-22 17:13:01 +0200

    t

--- main.py
+++ main.py
@@ -97,11 +97,11 @@ def run(dataset: DatasetChoice,
     elif dataset == DatasetChoice.FASHION:
         model = UnetSmall()
 
-    print("Training the model... ")
-    ddpm.train(model, dataloader, experiment_name, device, timesteps, epochs=epochs, lr=lr)
+    #print("Training the model... ")
+    #ddpm.train(model, dataloader, experiment_name, device, timesteps, epochs=epochs, lr=lr)
     
-    #print("Loading model weights")
-    #model.load_weights(path="results/conditional_test_1/final_TestUnet_CARRA2-siconc_batchsize4_t1500_epochs1_lr0.0003.pth" ,device="mps")
+    print("Loading model weights")
+    model.load_weights(path="results/full_conditional_10epochs/final_Unet_CARRA2-siconc_batchsize4_t1000_epochs10_lr0.0003.pth", device=device)
 
     return model, ddpm, train_ds
 
